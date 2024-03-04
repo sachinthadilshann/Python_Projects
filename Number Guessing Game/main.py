50accc21e3dc4114730ac71fcf3c46412d207e28
Sachintha Dilshan <sachinthadilshann@gmail.com>
2024-03-04 15:01:00 +0530

    Number Guessing Game

---
+++ Number Guessing Game/main.py
@@ -0,0 +1,11 @@
+from random import randint
+
+lower_num , higest_num = 1,10
+
+random_num: int = randint(lower_num,higest_num)
+print(f'Guss the number in the range from {lower_num}to {higest_num}')
+
+while True:
+
+    try:
+        user_gu
